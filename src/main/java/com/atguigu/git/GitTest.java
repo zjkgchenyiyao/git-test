9312d38ae8f2031c8a5cbb496082ebfce6cbbda6
chenyiyao <1921893591@qq.com>
2022-02-27 01:48:32 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git!");
     }
 }

f159fc14370a86bf1fa236a6da03195076908ae1
chenyiyao <1921893591@qq.com>
2022-02-27 02:12:42 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello git!");
+        System.out.println("Hello git3!");
+        System.out.println("Hello git4!");
     }
 }

289390b63ff758b02cb19030063826c12ea70d0c
chenyiyao <1921893591@qq.com>
2022-02-27 01:32:22 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author chenyiyao
+ * @create 2022-02-27 1:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
